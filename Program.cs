3bf89fc2d306cd7703305857144c84dfd88f4416
Vivek <svivek1507@gmail.com>
2023-10-03 22:20:14 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecialChar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            int alpha, digit, speChar, i, length;
+            alpha = digit = speChar = i = 0;
+            Console.WriteLine("Enter the String : ");
+            str = Console.ReadLine();
+            length = str.Length;
+            while(i< length) 
+            {
+                if ((str[i]>='a' && str[i]<='z') || (str[i]>='A' && str[i]<='Z'))
+                {
+                    alpha++;
+                }
+                else if (str[i]>='0' && str[i]<='9')
+                {
+                    digit++;
+                }
+                else
+                {
+                    speChar++;
+                }  
+                i++;
+            }
+            Console.WriteLine("Alphabets available in given string : {0}", alpha);
+            Console.WriteLine("Digits available in given string : {0}", digit);
+            Console.WriteLine("Special character available in given string : {0}", speChar);
+            Console.ReadLine();
+        }
+    }
+}
